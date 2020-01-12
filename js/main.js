7047d851185600fca0e95dd7e4c491c226973c52
Benjamin Holczer <bholc001@gold.ac.uk>
2020-01-12 21:03:53 +0000

    all file added

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// Opens the menu on a phone
+
+$(document).ready(function() 
+                  {
+    $(".menu-icon").on("click", function()
+                       {
+        $("nav ul").toggleClass("open");
+    });
+});
+
+// Slideshow controlling on the home page.
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("top-news");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+}
